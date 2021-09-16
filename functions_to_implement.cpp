fac4b83cc779209fbde411c4c7454d18830d0ed2
spencernikolaeff <59879326+spencernikolaeff@users.noreply.github.com>
2021-09-16 13:58:37 -0600

    spencer does a change for step 7

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -1,7 +1,7 @@
 /* String functions section */
 
 // Splits a single string on separator into a vector of strings
-std::vector<std::string> Split(std::string whole, std::string separator);
+std::vector<std::string> Split(std::string whole, std::string separator); //STEP 7 MODIFY THE CONTENTS
 
 // takes two strings and returns a new string that is the result of removing all occurrences of s2 from s1.
 std::string RemoveAllSubstrings(std::string s1, std::string s2);
