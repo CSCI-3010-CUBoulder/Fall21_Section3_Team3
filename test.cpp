bcfbc192a2171902bda53a6fb381b8ad19b1e14b
jona9117 <70180212+jona9117@users.noreply.github.com>
2021-09-21 13:33:29 -0600

    added factorials test case

--- test.cpp
+++ test.cpp
@@ -2,3 +2,12 @@
 #include "catch.hpp"
 #include "functions_to_implement.cpp"
 #include <vector>
+
+
+TEST_CASE ( "Factorials are tested", "[factorial]") {
+  REQUIRE( Factorial(0) == 1 );
+  REQUIRE( Factorial(1) == 1 );
+  REQUIRE( Factorial(2) == 2 );
+  REQUIRE( Factorial(4) == 24 );
+  REQUIRE( Factorial(11) == 39916800 );
+}
